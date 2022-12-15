05f163020cb785fb4d8a65d2bf4f8d42eff2541c
s.djuraev <s.djuraev@newuzbekistanuniversity.uz>
2022-12-15 09:45:28 +0500

    V1

--- src/main/java/com/example/databaseconnectionexample/HelloController.java
+++ src/main/java/com/example/databaseconnectionexample/HelloController.java
@@ -1,14 +1,30 @@
 package com.example.databaseconnectionexample;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    Button register;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void setRegister() throws IOException {
+        Stage stage=new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("registration-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400, 400);
+        stage.initModality(Modality.APPLICATION_MODAL);
+        stage.setTitle("Registration");
+        stage.setScene(scene);
+        stage.show();
     }
+
 }
